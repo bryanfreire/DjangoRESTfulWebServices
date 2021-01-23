1e12586bc626b68167c426c8ae0046f5c93ce364
Bryan Freire <bryanfv95@gmail.com>
2021-01-23 15:54:46 +0100

    Fix toy model and add migration

--- 01/restful01/toys/models.py
+++ 01/restful01/toys/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Toy(Model):
+class Toy(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length = 150, blank=False, default='')
     description = models.CharField(max_length = 250, blank=True, default='')
