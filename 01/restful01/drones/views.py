2aa94496c6aad7b2f9b8b7afcfe1e285ed7d35a2
Bryan Freire <bryanfv95@gmail.com>
2021-01-24 21:50:31 +0100

    Add serializers and views

--- 01/restful01/drones/views.py
+++ 01/restful01/drones/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
-# Create your views here.
+from .models import DroneCategory
+from .models import Drone
+from .models import Pilot
+from .models import Competition
+
+from .serializers import DroneCategorySerializer
+from .serializers import DroneSerializer
+from .serializers import PilotSerializer
+from .serializers import PilotCompetitionSerializer
+
+
+class DroneCategoryList(ListCreateAPIView):
+    queryset = DroneCategory.objects.all()
+    serializer_class = DroneCategorySerializer
+    name = 'dronecategory-list'
+
+
+class DroneCategoryDetail(RetrieveUpdateDestroyAPIView):
+    queryset = DroneCategory.objects.all()
+    serializer_class = DroneCategorySerializer
+    name = 'dronecategory-detail'
+
+
+class DroneList(ListCreateAPIView):
+    queryset = Dronte.objects.all()
+    serializer_class = DronteSerializer
+    name = 'drone-list'
+
+
+class DroneDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+    name = 'drone-detail'
+
+
+class PilotList(ListCreateAPIView):
+    queryset = Pilot.objects.all()
+    serializer_class = PilotSerializer
+    name = 'pilot-list'
+
+
+class PilotDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Pilot.objects.all()
+    serializer_class = PilotSerializer
+    name = 'pilot-detail'
+
+
+class CompetitionList(ListCreateAPIView):
+    queryset = Competition.objects.all()
+    serializer_class = PilotCompetitionSerializer
+    name = 'competition'
+
+
+class CompetitionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Competition.objects.all()
+    serializer_class = PilotCompetitionSerializer
+    name = 'competition-detail'
